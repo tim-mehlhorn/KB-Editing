066ee5f2689cb9a16ea9dd61f2301d30c5f2c4ed
Tim Mehlhorn <timmehlhorn@Tims-MacBook-Pro.local>
2019-08-31 09:35:44 -0500

    Footer styling and adjustments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,65 +8,58 @@ import instagram from '../img/social/instagram.svg'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="KB Editing"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-              <div className="columns">
-                <div className="column is-4">
-                <section className="menu">
-                    <ul className="menu-list">
-                      <li><Link to="/" className="navbar-item">Home</Link></li>
-                      <li><Link className="navbar-item" to="/about">About</Link></li>
-                    <li><a
-                      className="navbar-item"
-                      href="/admin/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Admin
-                    </a></li>
-                    </ul>
-                  </section>
-                </div>
-                <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                  <li>
-                    <Link className="navbar-item" to="/contact">
-                      Contact
-                    </Link>
-                  </li>
-                  </ul>
-                </section>
-                </div>
-                <div className="column is-4 social">
-                
-                  <a title="facebook" href="https://facebook.com">
-                    <img 
-                      src={facebook}
-                      alt="Facebook"
-                      style={{ width: '1em', height: '1em' }}
+      <footer className="footer has-text-white-ter">
+        <div className="content has-text-centered has-text-white-ter">
+          <div className="container has-text-white-ter">
+            <div className="columns">
+              <div className="column is-2">
+                <div className="content has-text-centered">
+                  <Link to="/">
+                    <img
+                      src={logo}
+                      alt="KB Editing"
+                      style={{ width: '6em', height: '6em' }}
                     />
-                  </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
+                  </Link>
+                </div>
+              </div>
+              <div className="column is-6">
+              <section className="menu">
+                <ul className="menu-list sitemap">
+                  <li><Link to="/" className="navbar-item">Home</Link></li>
+                  <li><Link className="navbar-item" to="/about">About</Link></li>
+                  <li><Link className="navbar-item" to="/contact">Contact</Link></li>
+                  <li><a className="navbar-item" href="https://www.google.com" target="_blank">Author Site</a></li>
+                  <li><a
+                    className="navbar-item"
+                    href="/admin/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >Admin</a></li>
+                </ul>
+              </section>
+            </div>
+            <div className="column is-3 social">
+              <h4>Connect with KB Editing</h4>
+              <a title="facebook" href="https://facebook.com">
+                <img
+                  src={facebook}
+                  alt="Facebook"
+                  style={{ width: '1em', height: '1em' }}
+                />
+              </a>
+              <a title="instagram" href="https://instagram.com">
+                <img
                   src={instagram}
                   alt="Instagram"
                   style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                </div>
-              </div>
+                />
+              </a>
             </div>
+          </div>
         </div>
-      </footer>
+        </div>
+      </footer >
     )
   }
 }
