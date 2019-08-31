7a492738c01140314885971f9bbf06bcd6ebbf1f
Tim Mehlhorn <timmehlhorn@Tims-MacBook-Pro.local>
2019-08-31 08:42:49 -0500

    Fixing build

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
-const NotFoundPage = () => (
+export default class notFound extends React.Component {
+  render() {
+    return (
   <Layout>
     <div>
       <h1>NOT FOUND</h1>
@@ -9,5 +11,4 @@ const NotFoundPage = () => (
     </div>
   </Layout>
 )
-
-export default NotFoundPage
+    }};
